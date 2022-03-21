308e7169f86c675f36a702b46d098164eea87ee2
Lucas Barbosa <lucas.chu.barbosa@gmail.com>
2022-03-21 22:31:41 +1100

    made the smart decision to switch to python

--- src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-axios.defaults.params = {};
-axios.defaults.params["apikey"] = process.env.ALPHA_VANTAGE_API_KEY;
-
-export default axios.create({
-  baseURL: "https://www.alphavantage.co",
-});

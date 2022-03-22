130a9342ed82575f364643cc5610b3103848a03d
Lucas Barbosa <lucas.chu.barbosa@gmail.com>
2022-03-22 11:53:06 +1100

    adding nodejs server to retrieve crypto information

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+import asyncio
+import json
+import websockets
+
+cc = "btcusd"
+interval = "1m"
+socket = f"wss://stream.binance.com:9443/ws/{cc}t@kline_{interval}"
+print(socket)
+
+
+ws = websockets.connect("wss://stream.binance.com:9443/ws/{cc}t@kline_{interval}")
+print(ws)

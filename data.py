3d2aeef526294bf13692ec8dce7b652fbcb0f9fc
Lucas Barbosa <lucas.chu.barbosa@gmail.com>
2022-03-27 22:13:35 +1100

    deleted and renamed files fixing a lot of runtime bugs

--- data.py
+++ data.py
@@ -1,9 +1,70 @@
-# Back testing historical data with threads
+# Collection and storage of Market data
 
+import json
+import pprint
+import sys
+import websocket
 import csv
 import threading
 from datetime import datetime
 from client import BinanceClient, Client
+
+
+# Web sockets
+def on_open(ws):
+    print("Connection opened...")
+
+
+def on_close(ws):
+    sys.exit()
+
+
+def on_message(ws, message):
+    res = json.loads(message)
+
+    data_obj = {
+        # meta data
+        "event_type": res["e"],
+        "event_time": res["E"],
+        "symbol": res["s"],
+        # market data
+        "kline_start_time": res["k"]["t"],
+        "kline_close_time": res["k"]["T"],
+        "interval": res["k"]["i"],
+        "first_trade_id": res["k"]["f"],
+        "last_trade_id": res["k"]["L"],
+        "open_price": res["k"]["o"],
+        "close_price": res["k"]["c"],
+        "high_price": res["k"]["h"],
+        "low_price": res["k"]["l"],
+        "base_asset_volume": res["k"]["v"],
+        "number_of_trades": res["k"]["n"],
+        "is_this_kline_closed": res["k"]["x"],
+        "quote_asset_volume": res["k"]["q"],
+        "taker_buy_base_asset_volume": res["k"]["V"],
+        "taker_buy_quote_asset_volume": res["k"]["Q"],
+    }
+
+    filename = f'data/csv/{res["s"]}.csv'  # e.g. data/csv/BTCUSDT.csv
+    with open(filename, "a", encoding="UTF8", newline="") as csvfile:
+        csv_writer = csv.DictWriter(csvfile, fieldnames=data_obj.keys())
+        # if not os.path.isfile(filename):
+
+        csv_writer.writerow(data_obj)
+
+    pprint.pprint(data_obj)
+
+
+def spin_ws(symbol="btcusdt", interval="1m"):
+    socketAddr = f"wss://stream.binance.com:9443/ws/{symbol}@kline_{interval}"
+    ws = websocket.WebSocketApp(socketAddr, on_open=on_open, on_close=on_close, on_message=on_message)
+    ws.run_forever()
+
+
+def get_wss_kline(symbol_list, interval):
+    threads = [threading.Thread(target=spin_ws, args=[symbol, interval]) for symbol in symbol_list]
+    [t.start() for t in threads]
+    [t.join() for t in threads]
 
 
 table_header = [
@@ -35,6 +96,11 @@ def write_csv(file, data):
 
 
 if __name__ == "__main__":
+    # (1) Websocket
+    symbol_list = ["btcusdt", "ethusdt"]
+    get_wss_kline(symbol_list, "1m")
+
+    # (2) Binance client api
     start_time = datetime.now()
     btc_usd_data = BinanceClient.get_klines(symbol="BTCBUSD", interval=Client.KLINE_INTERVAL_15MINUTE)
     # historical_data = BinanceClient.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "1 Jan, 2012", "1 Jan, 2022")
--- main.py
+++ main.py
@@ -1,1 +1,103 @@
-from client import BinanceClient, Client
+import math
+
+import numpy as np
+import pandas as pd
+import pandas_ta as pta
+
+from ta.trend import VortexIndicator
+from datetime import datetime
+from simple_chalk import chalk
+
+
+def vortex_1(df, n):
+    """Custom implementation, inspired off TradingView Pinescript example
+
+    Args:
+        df (pandas.DataFrame): market data
+        n (int): period of analysis
+    """
+    high = df["High"]
+    low = df["Low"]
+    close = df["Close"]
+    time = df["Open Time"]
+
+    n = 3
+    TR = pta.ATR(high, low, close)
+    STR = TR.rolling(n).sum()
+
+    for i in range(1, len(low)):
+        VMP = math.fabs(high[i] - low[i - 1])
+        VMM = math.fabs(low[i] - high[i - 1])
+        VIP = VMP / STR[i - 1]
+        VIM = VMM / STR[i - 1]
+
+        curr_time = int(time[i]) / 1000
+        curr_time = datetime.utcfromtimestamp(curr_time).strftime("%Y-%m-%d %H:%M:%S")
+        if VIP > VIM:
+            print(curr_time, chalk.green.bold("| buy  |"), end=" ")
+        else:
+            print(curr_time, chalk.red.bold("| sell |"), end=" ")
+
+        print(f"VIP = {VIP}, VIM = {VIM}")
+
+
+def vortex_2(df, n):
+    """Naive implementation found on the internet. Does not output VIP and VIM values
+
+    Args:
+        df (pandas.DataFrame): market data
+        n (int): period of analysis
+
+    Returns:
+        pandas.DataFrame: Vortex indicator results
+    """
+    i = 0
+    TR = [0]
+    while i < df.index[-1]:
+        Range = max(df.at[i + 1, "High"], df.at[i, "Close"]) - min(df.at[i + 1, "Low"], df.at[i, "Close"])
+        TR.append(Range)
+        i = i + 1
+    i = 0
+    VM = [0]
+    while i < df.index[-1]:
+        Range = abs(df.at[i + 1, "High"] - df.at[i, "Low"]) - abs(df.at[i + 1, "Low"] - df.at[i, "High"])
+        VM.append(Range)
+        i = i + 1
+
+    VI = pd.Series(pd.Series(VM).rolling(n).sum() / pd.Series(TR).rolling(n).sum(), name="Vortex_" + str(n))
+    df = df.join(VI)
+    return df
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv("data/csv/btcusd_15min.csv", sep=",")
+
+    pandas_vtx = pta.vortex(df["High"], df["Low"], df["Close"], 3)
+    ta_vtx = VortexIndicator(df["High"], df["Low"], df["Close"], 3)
+
+    df["VTXP_3_Pandas"] = pandas_vtx["VTXP_3"]
+    df["VTXM_3_Pandas"] = pandas_vtx["VTXM_3"]
+    df["VTXP_3_Ta"] = ta_vtx.vortex_indicator_pos()
+    df["VTXM_3_Ta"] = ta_vtx.vortex_indicator_neg()
+
+    for idx, row in df.iterrows():
+        curr_time = int(row["Open Time"]) / 1000
+        curr_time = datetime.utcfromtimestamp(curr_time).strftime("%Y-%m-%d %H:%M:%S")
+
+        if row["VTXP_3_Ta"] > row["VTXM_3_Ta"]:
+            print(
+                chalk.yellow("pta"),
+                curr_time,
+                chalk.green.bold("| buy |"),
+                f'VIP = {row["VTXP_3_Pandas"]}, VIM = {row["VTXM_3_Pandas"]}',
+            )
+            print("ta ", curr_time, chalk.green.bold("| buy |"), f'VIP = {row["VTXP_3_Ta"]}, VIM = {row["VTXM_3_Ta"]}')
+        else:
+            print(
+                chalk.yellow("pta"),
+                curr_time,
+                chalk.red.bold("| buy |"),
+                f'VIP = {row["VTXP_3_Pandas"]}, VIM = {row["VTXM_3_Pandas"]}',
+            )
+            print("ta ", curr_time, chalk.red.bold("| buy |"), f'VIP = {row["VTXP_3_Ta"]}, VIM = {row["VTXM_3_Ta"]}')
